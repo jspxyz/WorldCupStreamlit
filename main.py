7d37d33cee004bf5279cbcfb886e0a8f0f6e98a4
jswpark <jswpark@gmail.com>
2020-10-17 14:50:20 +0700

    updated csv to be lower case longitude latitude. updated working_main.py file with working geomapgit add .
    git status

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
+import geopandas
 import matplotlib.pyplot as plt
 import streamlit as st
-import geopandas
 
 # 0.0 reading datafiles
 matches = pd.read_csv('matches.csv', encoding='latin1')
