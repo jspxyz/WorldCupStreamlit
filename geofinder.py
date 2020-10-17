608d9fc41a74e5d030858d8cfdde62b945df626c
jswpark <jswpark@gmail.com>
2020-10-17 18:10:01 +0700

    cleaned up geofinder.py file. will not run again though for this project. at least, i hope i don't need to.

--- geofinder.py
+++ geofinder.py
@@ -6,26 +6,28 @@ import streamlit as st
 from opencage.geocoder import OpenCageGeocode
 # from pprint import pprint
 
-# testing opencage city finder
+# key for geocoder
 key = '00ad2adcfadd4b32b3612bd75c680741'
 geocoder = OpenCageGeocode(key)
+
+# testing opencage city finder
 # query = 'Bijuesca, Spain'  
 # results = geocoder.geocode(query)
 # lat = results[0]['geometry']['lat']
 # lng = results[0]['geometry']['lng']
 # print (lat, lng)
 
-# matches = pd.read_csv('matches.csv', encoding='latin1')
+matches = pd.read_csv('matches.csv', encoding='latin1')
 
-# # create empty lists
+# create empty lists
 list_lat = []   
 list_long = []
-	
-# for index, row in matches.iterrows(): # iterate over rows in dataframe
 
-    City = row['City'].strip()
-    State = row['Host Country']
-    query = str(City)+','+str(State)
+# iterate over rows in dataframe
+for index, row in matches.iterrows(): 
+    city = row['City'].strip()
+    state = row['Host Country']
+    query = str(city)+','+str(state)
     print(query)
 
     results = geocoder.geocode(query)
@@ -38,8 +40,7 @@ list_long = []
 
 print(list_lat)
 print(list_long)
+
 # create new columns from lists    
-
 matches['latitude'] = list_lat   
-
 matches['longitude'] = list_long
