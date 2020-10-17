949404ea87dcad63157f94f4fb13be5d0fc48faf
jswpark <jswpark@gmail.com>
2020-10-17 09:17:40 +0700

    added graphs for attendance. added working_main.py for JP to work on.

--- working_main.py
+++ working_main.py
@@ -213,3 +213,17 @@ st.pyplot(fig)
 # price_house_type = df[df['neighborhood_group'] ==
 #                     str(user_area)].groupby('room_type').mean()['price']
 # st.dataframe(price_house_type)
+
+df = pd.DataFrame(
+    {'City': ['Buenos Aires', 'Brasilia', 'Santiago', 'Bogota', 'Caracas'],
+     'Country': ['Argentina', 'Brazil', 'Chile', 'Colombia', 'Venezuela'],
+     'Latitude': [-34.58, -15.78, -33.45, 4.60, 10.48],
+     'Longitude': [-58.66, -47.91, -70.66, -74.08, -66.86]})
+gdf = geopandas.GeoDataFrame(
+    df, geometry=geopandas.points_from_xy(df.Longitude, df.Latitude))
+st.write(gdf.head())
+world = geopandas.read_file(geopandas.datasets.get_path('naturalearth_lowres'))
+ax = world[world.continent == 'South America'].plot(
+    color='white', edgecolor='black')
+gdf.plot(ax=ax, color='red')
+st.pyplot()
