a9f66640ad1d352955b3d506a3e3646cdf824667
jswpark <jswpark@gmail.com>
2020-10-18 16:43:42 +0700

    lots of updates to analysis.py. now it's running on streamlit. yes. go go

--- analysis.py
+++ analysis.py
@@ -8,86 +8,40 @@ import matplotlib as mpl
 import streamlit as st 
 
 
+# Loading Data
 @st.cache(allow_output_mutation=True)
 def load_data(path):
     data = pd.read_csv(path)
     return data
 
-# Chance of host country in Top 3
+matches = load_data('/Users/jsp/Desktop/KermadecProjects/WorldCupStreamlit/matches_clean.csv')
 
-winners_final = load_data('/Users/jsp/Desktop/KermadecProjects/WorldCupStreamlit/winners_final.csv')
-st.markdown('Chance that host country in Top 3')
-st.text(winners_final[['Hostwinner', 'Hosttop3']].mean())
-if st.checkbox('Show winner raw data'):
-    st.subheader('Raw data')
-    st.dataframe(winners_final)
-
-
-# Map of top 3
-Top_3 = load_data('/Users/jsp/Desktop/KermadecProjects/WorldCupStreamlit/Top3.csv')
-
-st.subheader('Where are the World Cup Winners')
-# Plot winner on map
-slide = st.slider("Winning time", 1, 5)
-winner = Top_3[(Top_3['Winner'] >= int(slide))]
-st.map(winner,1.3)
-
-# Detailed podium
-st.subheader('How well were they doing?')
-option = st.selectbox('Select a country you love', Top_3['Countries'].unique())
-Country_Achievement = Top_3[Top_3['Countries'] == str(option)][['Countries', 'Winner','Runners-Up','Third']]
-st.table(Country_Achievement)
-
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(Top_3)
+#-----
+# introsection
+st.title('World Cup Analysis | October 2020')
+st.subheader('Data from 1930 - 2014')
+st.subheader('by Rosy, Jun, James')
 
 
-# How was each country perform in all World Cup?
-st.subheader('How was a Country\'s performance?')
-# Select box
-Brazil = load_data('/Users/jsp/Desktop/KermadecProjects/WorldCupStreamlit/Brazil.csv')
+# Total of attendance and goals by year
+st.header('Goal Insights')
+st.write('blah blah blah')
+summatches = matches.groupby('Year').sum()
+attendance = summatches['Attendance']
+goals = summatches['GoalsTotal']
 
-# Grouped bar chart for Brazil
-
-year = Brazil['Year'].tolist()
-number_of_match = Brazil['number_of_match'].tolist()
-winning_match = Brazil['wining_match'].tolist()
-x = np.arange(len(year))  
-width = 0.35 
-fig, ax = plt.subplots(figsize=(17,7))
-rects1 = ax.bar(x - width/2, number_of_match, width, label='number of match')
-rects2 = ax.bar(x + width/2, winning_match, width, label='winning match')
-ax.set_ylabel('Match')
-ax.set_xlabel('Year')
-ax.set_title('Brazil Performance in World Cup')
-ax.set_xticks(Brazil.index)
-ax.set_xticklabels(year)
-ax.legend()
-
-def autolabel(rects):
-    """Attach a text label above each bar in *rects*, displaying its height."""
-    for rect in rects:
-        height = rect.get_height()
-        ax.annotate('{}'.format(height),
-                    xy=(rect.get_x() + rect.get_width() / 2, height),
-                    xytext=(0, 3),  # 3 points vertical offset
-                    textcoords="offset points",
-                    ha='center', va='bottom')
-
-autolabel(rects1)
-autolabel(rects2)
+fig = plt.figure(figsize=(3, 3))
+plt.scatter(attendance, goals)
+plt.xlabel('Attendance')
+plt.xlim(0,4000000)
+plt.ylabel('Goals')
+plt.title('Attendance & Goals Correlation')
 fig.tight_layout()
 st.pyplot(fig)
 
-if st.checkbox('Show performance raw data'):
-    st.subheader('Raw data')
-    st.dataframe(Brazil)
-
-
 
 # Number of match and Average goal chart
-st.subheader('How were their performance?')
+st.subheader('Matches and Goals Data')
 Average_goal = load_data('/Users/jsp/Desktop/KermadecProjects/WorldCupStreamlit/Average_goal.csv')
 
 year = Average_goal['Year'].tolist()
@@ -114,15 +68,184 @@ ax2.tick_params(axis='y', labelcolor=color)
 st.pyplot(fig)
 
 # Showing raw data
-st.dataframe(Average_goal)
+if st.checkbox('Show raw data for Matches and Goals Data above:'):
+    st.subheader('Raw data')
+    st.write(Average_goal)
+
+
+st.header('Champion Insights')
+st.write('blahblaskdfjla;jef')
+
+
+# Map of top 3
+Top_3 = load_data('/Users/jsp/Desktop/KermadecProjects/WorldCupStreamlit/Top3.csv')
+
+st.subheader('Where are the World Cup Winners')
+# Plot winner on map
+slide = st.slider("Winning time", 1, 5)
+winner = Top_3[(Top_3['Winner'] >= int(slide))]
+st.map(winner,1.3)
+
+st.write('bleh blah lbuer ')
+
+# adding group by chart for country WON and Top3
+# Grouped bar chart show how many time each country won and in top 3
+Countryname = Top_3['Countries'].tolist()
+Winnerdata = Top_3['Winner'].tolist()
+Totaldata = Top_3['Totaltop3'].tolist()
+
+x = np.arange(len(Countryname))  
+width = 0.35 
+
+fig, ax = plt.subplots(figsize=(17,5))
+
+rects1 = ax.bar(x - width/2, Totaldata, width, label='Top 3')
+rects2 = ax.bar(x + width/2, Winnerdata, width, label='Winner')
+
+ax.set_ylabel('Number of occurence')
+ax.set_xlabel('Countries')
+ax.set_title('Country won and in top 3')
+ax.set_xticks(x)
+ax.set_xticklabels(Countryname)
+ax.legend()
+
+def autolabel(rects):
+    """Attach a text label above each bar in *rects*, displaying its height."""
+    for rect in rects:
+        height = rect.get_height()
+        ax.annotate('{}'.format(height),
+                    xy=(rect.get_x() + rect.get_width() / 2, height),
+                    xytext=(0, 3),  # 3 points vertical offset
+                    textcoords="offset points",
+                    ha='center', va='bottom')
+
+
+autolabel(rects1)
+autolabel(rects2)
+
+fig.tight_layout()
+
+st.pyplot(fig)
+
+
+
+# New Section--------------------------------
+# How was each country perform in all World Cup?
+st.subheader('Each Country\'s overall individual performance.')
+
+#function to get country details
+def countryDetails(country):
+    # setting datetime
+    matches['Datetime'] = pd.to_datetime(matches['Datetime'])
+
+    # setting years to Ints and using year as index
+    matches['Year']= matches['Year'].astype('int')
+    matches.set_index('Year')
+
+    # finding all matches played by Brazil
+    country_matches = matches[(matches['Home Team Name']==country) | (matches['Away Team Name']==country)]
+
+    # reset index to year
+    country_matches.set_index('Year')
+
+    # finding number of matches played by Brazil by year
+    country_match = country_matches.groupby('Year').agg(number_of_match=('City','count'))
+
+    # finding interesting win conditions
+    country_matches['Win conditions'].unique()
+
+    # finding Brazil winning matches
+    Country_winning_matches = country_matches[country_matches['WinningTeam']==country].groupby('Year').agg(wining_match=('WinningTeam','count'))
+
+    # creating Brazil Table of number of matches played and matches won
+    country_table_final = country_match.merge(Country_winning_matches , how='left', left_index=True, right_on='Year')
+    country_table_final.set_index('Year',inplace=True)
+    country_table_final.fillna('0',inplace=True)
+
+    return country_table_final
+
+
+option = st.selectbox('Select a country for addtional details', ('Argentina', 'Austria', 'Brazil', 'Chile', 'Germany', 'Italy', 'Spain', 'France', 'England', ' Netherlands', 'Czechoslovakia', 'Hungary', 'Sweden', 'Poland', 'USA', 'Croatia', 'Turkey', 'Portugal', 'Uruguay'))
+Country_Details = countryDetails(str(option))
+st.table(Country_Details)
+
+
+# Select box
+selectCountry = Country_Details
+
+# Grouped bar chart for selectCountry
+selectCountry.reset_index(level = 0, inplace=True)
+selectCountry.columns = ['Year', 'number_of_match', 'wining_match']
+selectCountry['wining_match'] = selectCountry['wining_match'].astype(int)
+
+year = selectCountry['Year'].tolist()
+number_of_match = selectCountry['number_of_match'].tolist()
+winning_match = selectCountry['wining_match'].tolist()
+x = np.arange(len(year))
+width = 0.35 
+fig, ax = plt.subplots(figsize=(17,7))
+rects1 = ax.bar(x - width/2, number_of_match, width, label='number of match')
+rects2 = ax.bar(x + width/2, winning_match, width, label='winning match')
+ax.set_ylabel('Match')
+ax.set_xlabel('Year')
+ax.set_title('Performance in World Cup')
+ax.set_xticks(selectCountry.index)
+ax.set_xticklabels(year)
+ax.legend()
+
+def autolabel(rects):
+    """Attach a text label above each bar in *rects*, displaying its height."""
+    for rect in rects:
+        height = rect.get_height()
+        ax.annotate('{}'.format(height),
+                    xy=(rect.get_x() + rect.get_width() / 2, height),
+                    xytext=(0, 3),  # 3 points vertical offset
+                    textcoords="offset points",
+                    ha='center', va='bottom')
+
+autolabel(rects1)
+autolabel(rects2)
+fig.tight_layout()
+st.pyplot(fig)
+
+if st.checkbox('Show performance raw data'):
+    st.subheader('Raw data')
+    st.dataframe(selectCountry)
+
+
+st.header('Recommendations')
+# Chance of host country in Top 3
+
+winners_final = load_data('/Users/jsp/Desktop/KermadecProjects/WorldCupStreamlit/winners_final.csv')
+st.markdown('Chance that host country in Top 3')
+st.text(winners_final[['Hostwinner', 'Hosttop3']].mean())
+if st.checkbox('Show winner raw data'):
+    st.subheader('Raw data')
+    st.dataframe(winners_final)
+
+st.markdown('Breakdown of all countries that have placed in the Top 3')
+st.table(Top_3[['Countries','Winner','Runners-Up','Third']])
+
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(Top_3)
 
 
 
 
+#------------------
+# additional code not used but left for examples
 
+# Detailed podium
+# st.subheader('How well were they doing?')
+# option = st.selectbox('Select a country you love', Top_3['Countries'].unique())
+# Country_Achievement = Top_3[Top_3['Countries'] == str(option)][['Countries', 'Winner','Runners-Up','Third']]
+# st.table(Country_Achievement)
 
-
-
+# if st.checkbox('Show raw data'):
+#     st.subheader('Raw data')
+#     st.write(Top_3)
+#------------------
 
 
 # st.sidebar.header('User Input Features')
@@ -153,3 +276,20 @@ st.dataframe(Average_goal)
 # >>>
 # >>> col2.subheader("A narrow column with the data")
 # >>> col2.write(data)
+#------------------
+
+
+
+
+# example selector box
+# option = st.selectbox('Select a country you love', Top_3['Countries'].unique())
+# Country_Achievement = Top_3[Top_3['Countries'] == str(option)][['Countries', 'Winner','Runners-Up','Third']]
+# st.table(Country_Achievement)
+
+# example 2 selector box
+# st.subheader('Price by neighborhood example')
+# st.selectbox('Select an area', ('Brooklyn','Manhattan', 'Queens', 'Bronx', 'Staten Island'))
+# price per type
+# price_house_type = df[df['neighborhood_group'] ==
+#                     str(user_area)].groupby('room_type').mean()['price']
+# st.dataframe(price_house_type)
